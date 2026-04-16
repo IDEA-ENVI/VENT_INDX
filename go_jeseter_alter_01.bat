6882fb02a4ed1e09a0df3f9e353e26c00fd6f812
nov683 <77004804+nov683@users.noreply.github.com>
2026-04-16 14:41:24 +0200

    Plnění úkolu č.8 za 2.q.2026

---
+++ go_jeseter_alter_01.bat
@@ -0,0 +1,2 @@
+c:\app\OraQ\OraQ.exe /cc:vm_vix /q:alter_01.sql                    /log:alter_01.log          
+exit;
---
+++ go_praha_alter_01.bat
@@ -0,0 +1,2 @@
+c:\app\OraQ\OraQ.exe /cc:pha_vix /q:alter_01.sql                    /log:alter_01.log          
+exit;
