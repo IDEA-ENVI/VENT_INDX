5c7e1896db0e19ce5a7ef0d6767e422d8e3bc712
nov683 <77004804+nov683@users.noreply.github.com>
2026-05-26 08:22:57 +0200

    ukol c.9 za 2.q.2026

---
+++ go_praha_2026_2q_1.bat
@@ -0,0 +1,3 @@
+c:\app\OraQ\OraQ.exe /cc:pha_vix  /q:create_2026_2q_1.sql      /log:create_2026_2q_1.log          
+c:\app\OraQ\OraQ.exe /cc:pha_vix  /q:insert_2026_2q_1.sql      /log:insert_2026_2q_1.log          
+exit;
